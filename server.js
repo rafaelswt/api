8315ef75fca20b70df9196ac1c06d9d611438908
rafael ribeiro <54046383+rafaelswt@users.noreply.github.com>
2022-12-06 23:53:22 -0300

    update origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 var corsOptions = {
-  origin: "*"
+  origin: "https://www.aupamatch.com/"
 };
 app.use(cors(corsOptions));
 
