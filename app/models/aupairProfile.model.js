ba37d8f368b198b6606639d927cc05e8c3a624f2
Rafael <rafael.carvalho137@gmail.com>
2023-03-26 20:40:42 -0300

    f

--- app/models/aupairProfile.model.js
+++ app/models/aupairProfile.model.js
@@ -7,72 +7,26 @@ const Aupair = mongoose.model(
     cep: String,
     logradouro: String,
     numero: String,
-    complemento: String,
     cidade: String,
     estado: String, 
-    data_de_nascimento: Date,
-    escolaridade: {
-      type: String,
-      enum: ["Ensino Médio", "Graduação", "Pós-Graduação"],
-    },
-    idiomas: {
-      type: [String],
-      enum: ["Inglês", "Espanhol", "Francês", "Alemão", "Italiano", "Português", "Outro"]
-    },
-    religiao: {
-      type: String,
-      enum: ["Cristianismo", "Islamismo", "Judaísmo", "Budismo", "Outra"]
-    },
-    genero: {
-      type: String,
-      enum: ["Masculino", "Feminino", "Outro"],
-    },
-    nacionalidade: {
-      type: String,
-      enum: ["Brasileira", "Americana", "Espanhola", "Portuguesa", "Outra"],
-    },
-    habilitacao: {
-      type: Boolean,
-      default: false
-    },
-    quantidade_criancas: {
-      type: String,
-      enum: ['Não especificado', '1', '2', '3 ou mais'],
-      default: ['Não especificado']
-    },
-    experiencia_trabalho: {
-      type: String,
-      enum: ['0', '1-3', '4-6', '7-10', '10+'],
-      default: ['0']
-    },
-    natacao: {
-      type: Boolean,
-      default: false
-    },
-    carro_exclusivo: {
-      type: Boolean,
-      default: false
-    },
-    receber_newsletter: {
-      type: Boolean,
-      default: false
-    },
-    data_disponibilidade: {
-      type: Date,
-      default: null
-    },
-    numero_identificacao_nacional: String,
+    data_de_nascimento: String,
+    genero: String,
+    cpf: String,
+    nacionalidade: String,
     resumo: String,
     passaporte: String,
-    habilitacao_pid: String,
-    firstLogin: {
-      type: Boolean,
-      default: false
-    },
-    user: [
+    habilitacao: Boolean,
+    quantidade_criancas: String,
+    experiencia_trabalho: String,
+    natacao: Boolean,
+    carro_exclusivo: Boolean,
+    receber_newsletter: Boolean,
+    data_disponibilidade: String,
+    escolaridade: String,
+    aupair: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "Aupair"
       }
     ]
   })
