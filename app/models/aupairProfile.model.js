0100b10898a62f8658fbe1ff56526fb588b79ad2
Rafael <rafael.carvalho137@gmail.com>
2023-03-26 20:47:39 -0300

    ok

--- app/models/aupairProfile.model.js
+++ app/models/aupairProfile.model.js
@@ -1,35 +1,81 @@
 const mongoose = require("mongoose");
 
-const Aupair = mongoose.model(
-  "Aupair",
+const AupairProfile = mongoose.model(
+  "AupairProfile",
   new mongoose.Schema({
     telefone: String,
     cep: String,
     logradouro: String,
     numero: String,
+    complemento: String,
     cidade: String,
     estado: String, 
-    data_de_nascimento: String,
-    genero: String,
-    cpf: String,
-    nacionalidade: String,
+    data_de_nascimento: Date,
+    escolaridade: {
+      type: String,
+      enum: ["Ensino Médio", "Graduação", "Pós-Graduação"],
+    },
+    idiomas: {
+      type: [String],
+      enum: ["Inglês", "Espanhol", "Francês", "Alemão", "Italiano", "Português", "Outro"]
+    },
+    religiao: {
+      type: String,
+      enum: ["Cristianismo", "Islamismo", "Judaísmo", "Budismo", "Outra"]
+    },
+    genero: {
+      type: String,
+      enum: ["Masculino", "Feminino", "Outro"],
+    },
+    nacionalidade: {
+      type: String,
+      enum: ["Brasileira", "Americana", "Espanhola", "Portuguesa", "Outra"],
+    },
+    habilitacao: {
+      type: Boolean,
+      default: false
+    },
+    quantidade_criancas: {
+      type: String,
+      enum: ['Não especificado', '1', '2', '3 ou mais'],
+      default: ['Não especificado']
+    },
+    experiencia_trabalho: {
+      type: String,
+      enum: ['0', '1-3', '4-6', '7-10', '10+'],
+      default: ['0']
+    },
+    natacao: {
+      type: Boolean,
+      default: false
+    },
+    carro_exclusivo: {
+      type: Boolean,
+      default: false
+    },
+    receber_newsletter: {
+      type: Boolean,
+      default: false
+    },
+    data_disponibilidade: {
+      type: Date,
+      default: null
+    },
+    numero_identificacao_nacional: String,
     resumo: String,
     passaporte: String,
-    habilitacao: Boolean,
-    quantidade_criancas: String,
-    experiencia_trabalho: String,
-    natacao: Boolean,
-    carro_exclusivo: Boolean,
-    receber_newsletter: Boolean,
-    data_disponibilidade: String,
-    escolaridade: String,
-    aupair: [
+    habilitacao_pid: String,
+    firstLogin: {
+      type: Boolean,
+      default: false
+    },
+    user: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Aupair"
+        ref: "User"
       }
     ]
   })
 );
 
-module.exports = Aupair;
+module.exports = AupairProfile;
--- app/models/vaga.model.js
+++ app/models/vaga.model.js
@@ -1,30 +1,112 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "Vaga",
-  new mongoose.Schema({
-    escolaridade: String,
-    experiencia: String,
-    quantidade_criancas: String,
-    descricao: String,
-    natacao: Boolean,
-    habilitacao: Boolean,
-    carro_exclusivo: Boolean,
-    escolha: Boolean,
-    score : String,
-    user: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-      }
-    ],
-    aupair: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Aupair"
-      }
-    ]
-  })
-);
+const VagaSchema = new mongoose.Schema({
+  escolaridade: {
+    type: String,
+    enum: ["Qualquer Escolaridade", "Ensino Médio", "Graduação", "Pós-Graduação"],
+    default: "Qualquer Escolaridade"
+  },
+  idiomas: {
+    type: [String],
+    enum: ["Qualquer Idioma", "Inglês", "Espanhol", "Francês", "Alemão", "Italiano", "Português"],
+    default: ["Qualquer Idioma"]
+  },
+  religiao: {
+    type: String,
+    enum: ["Qualquer Religião", "Cristianismo", "Islamismo", "Judaísmo", "Budismo"],
+    default: "Qualquer Religião"
+  },
+  genero: {
+    type: String,
+    enum: ["Qualquer Gênero", "Masculino", "Feminino"],
+    default: "Qualquer Gênero"
+  },
+  nacionalidade: {
+    type: String,
+    enum: ["Qualquer Nacionalidade", "Brasileira", "Americana", "Espanhola", "Portuguesa", "Alemã", "Argentina", "Chinesa", "Japonesa"],
+    default: "Qualquer Nacionalidade"
+  },
+  faixa_etaria: {
+    type: [String],
+    enum: ["Qualquer Idade",'18-21', '22-30', '31-40', '41-50', '51+'],
+    default: ["Qualquer Idade"]
+  },
+  experiencia_trabalho: {
+    type: String,
+    enum: ['0', '1-3', '4-6', '7-10', '10+'],
+    default: '0'
+  },
+  quantidade_criancas: {
+    type: String,
+    enum: ['Não especificado', '1', '2', '3 ou mais'],
+    default: 'Não especificado'
+  },
+  resumo: String,
+  receber_newsletter: {
+    type: Boolean,
+    default: false
+  },
+  data_disponibilidade: {
+    type: Date,
+    default: null
+  },
+  data_finalizacao_vaga: {
+    type: Date,
+    default: null
+  },
+  titulo_vaga: {
+    type: String,
+    default: 'Vaga sem título'
+  },
+  vaga_patrocinada: {
+    type: Boolean,
+    default: false
+  },
+  pais: {
+    type: String,
+    default: ''
+  },
+  estado_provincia: {
+    type: String,
+    default: ''
+  },
+  descricao: {
+    type: String,
+    default: ''
+  },
+  natacao: {
+    type: Boolean,
+    default: false
+  },
+  habilitacao: {
+    type: Boolean,
+    default: false
+  },
+  carro_exclusivo: {
+    type: Boolean,
+    default: false
+  },
+  score: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  user: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  aupair: [{
+    aupairId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Aupair"
+    },
+    saved: {
+      type: Boolean,
+      default: false
+    }
+  }]
+}, { timestamps: true });
 
-module.exports = User;
+const Vaga = mongoose.model("Vaga", VagaSchema);
+
+module.exports = Vaga;
