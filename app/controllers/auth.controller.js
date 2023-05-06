4c44599bc2e359a8fae9e0d998a60878179d1c8a
Rafael <rafael.carvalho137@gmail.com>
2023-05-06 17:04:41 -0300

    Update auth.controller.js

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -146,6 +146,7 @@ exports.signin = async (req, res) => {
 
     // Obtém a localização do usuário a partir do endereço IP
     const ipAddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log(ipAddress)
     const ipstackApiKey = process.env.IPSTACK_API; // substitua pela sua API key do IP Geolocation API
     const ipstackApiUrl = `http://api.ipstack.com/${ipAddress}?access_key=${ipstackApiKey}`;
     const response = await axios.get(ipstackApiUrl);
